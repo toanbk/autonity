d7afbf2ed3ea26fb3c34f5a5ba77d2daa5d9cef1
Toan Pham <toan@betanet.co.il>
2023-12-21 12:56:11 +0700

    init script

---
+++ send.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# change sendto wallet
+# change pass your wallet
+
+sendto=`curl https://raw.githubusercontent.com/toanbk/autonity/main/wallet.txt`
+pass=123123
+
+echo "Today will set to wallet address: $sendto"
+
+n=1
+while :
+do
+     echo "send $n"
+     aut tx make --to $sendto --value 1  | aut tx sign --password $pass - | aut tx send - && sleep 3
+
+     n=$((n+1))
+
+done
+
